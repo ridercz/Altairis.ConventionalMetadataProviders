2074eef397d92a7947f01d06409af6d1655baa35
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-12-19 15:50:17 +0100

    Added request localizatio middleware

--- TestSite/Startup.cs
+++ TestSite/Startup.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Altairis.ConventionalMetadataProviders;
 using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace TestSite {
     public class Startup {
+        private static readonly CultureInfo[] _supportedCultures = {
+            new CultureInfo("en-US"),
+            new CultureInfo("cs-CZ"),
+        };
+
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc(options => {
                 options.SetConventionalMetadataProviders(typeof(Resources.Display));
@@ -18,10 +18,10 @@ namespace TestSite {
         }
 
         public void Configure(IApplicationBuilder app) {
-            app.Use((context, next) => {
-                CultureInfo.CurrentCulture = new CultureInfo("cs-CZ");
-                CultureInfo.CurrentUICulture = new CultureInfo("cs-CZ");
-                return next();
+            app.UseRequestLocalization(new RequestLocalizationOptions {
+                SupportedCultures = _supportedCultures,
+                SupportedUICultures = _supportedCultures,
+                DefaultRequestCulture = new RequestCulture(_supportedCultures[0]),
             });
             app.UseDeveloperExceptionPage();
             app.UseMvc();
