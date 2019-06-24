40b65a9b8f2ed1612b716e3694cfd32d5492d769
Jay.Skyworker <jayskyworker@gmail.com>
2019-06-24 20:43:58 +0800

    improve efficiency of resourceKeySuffix

--- Altairis.ConventionalMetadataProviders/ResourceManagerExtensions.cs
+++ Altairis.ConventionalMetadataProviders/ResourceManagerExtensions.cs
@@ -16,12 +16,21 @@ namespace Altairis.ConventionalMetadataProviders {
                 ? metadataIdentity.ContainerType.FullName + "." + metadataIdentity.Name
                 : metadataIdentity.Name;
 
+            if (string.IsNullOrWhiteSpace(resourceKeySuffix))
+            {
+                resourceKeySuffix = string.Empty;
+            }
+            else
+            {
+                resourceKeySuffix = "_" + resourceKeySuffix;
+            }
+
             // Search by name from more specific to less specific
             var nameParts = fullPropertyName.Split('.', '+');
             for (var i = 0; i < nameParts.Length; i++) {
                 // Get the resource key to lookup
                 var resourceKeyName = string.Join("_", nameParts.Skip(i));
-                if (!string.IsNullOrWhiteSpace(resourceKeySuffix)) resourceKeyName += "_" + resourceKeySuffix;
+                resourceKeyName += resourceKeySuffix;
 
                 // Check if given value exists in resource
                 if (resourceManager.GetString(resourceKeyName) != null) return resourceKeyName;
