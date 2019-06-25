c62e1b2a6549844ebed37d4bdfc397147ed99b18
Michal Altair Valášek <michal.valasek@altairis.cz>
2019-06-25 22:47:44 +0200

    Rewrote GetResourceKeyName to correctly handle suffixes

--- Altairis.ConventionalMetadataProviders/ResourceManagerExtensions.cs
+++ Altairis.ConventionalMetadataProviders/ResourceManagerExtensions.cs
@@ -8,28 +8,28 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 
 namespace Altairis.ConventionalMetadataProviders {
     internal static class ResourceManagerExtensions {
-        public static string GetResourceKeyName(this ResourceManager resourceManager, ModelMetadataIdentity metadataIdentity, string resourceKeySuffix) {
+        public static string GetResourceKeyName(this ResourceManager resourceManager, ModelMetadataIdentity metadataIdentity, string resourceKeySuffix, bool allowSuffixOnly = false) {
             if (resourceManager == null) throw new ArgumentNullException(nameof(resourceManager));
             if (string.IsNullOrEmpty(metadataIdentity.Name)) return null;
 
+            // Get full property name, ie. Namespace.Class.Property
             var fullPropertyName = !string.IsNullOrEmpty(metadataIdentity.ContainerType?.FullName)
                 ? metadataIdentity.ContainerType.FullName + "." + metadataIdentity.Name
                 : metadataIdentity.Name;
 
-            if (string.IsNullOrWhiteSpace(resourceKeySuffix))
-            {
-                resourceKeySuffix = string.Empty;
-            }
-            else
-            {
-                resourceKeySuffix = "_" + resourceKeySuffix;
+            // Start with resource key name equal to full property name, with "+" and "." replaced by "_"
+            var resourceKeyName = fullPropertyName.Replace('.', '_').Replace('+', '_');
+
+            // Get number of parts, ie. Namespace.Class.Property has 3
+            var namePartsCount = resourceKeyName.Length - resourceKeyName.Replace("_", string.Empty).Length + 1;
+
+            // Add suffix if specified
+            if (!string.IsNullOrWhiteSpace(resourceKeySuffix)) {
+                resourceKeyName += "_" + resourceKeySuffix;
+                if (allowSuffixOnly) namePartsCount++;
             }
 
             // Search by name from more specific to less specific
-            var resourceKeyName = fullPropertyName.Replace('.', '_').Replace('+', '_');
-            var namePartsCount = resourceKeyName.Length - resourceKeyName.Replace("_", string.Empty).Length + 1;
-            resourceKeyName += resourceKeySuffix;
-
             for (var i = 0; i < namePartsCount; i++) {
                 // Get the resource key to lookup
                 if (i > 0) resourceKeyName = resourceKeyName.Substring(resourceKeyName.IndexOf("_") + 1);
